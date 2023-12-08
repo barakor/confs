3b694e4f71c9bb654757618b5ac62f45e0e25b24
Barak <18008770+Lezyes@users.noreply.github.com>
2023-12-08 17:53:34 +0200

    this is annoying

---
+++ .screenlayout/monitor_only.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --dpi 120 --output DVI-D-0 --off --output HDMI-0 --off --output HDMI-1 --off --output DP-0 --off --output DP-1 --off --output DP-2 --primary --mode 3440x1440 --pos 0x0 --rotate normal --refresh 175.0 --output DP-3 --off
---
+++ .screenlayout/salon_only.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --dpi 180 --output DVI-D-0 --off --output HDMI-0 --primary --rate 60 --mode 3840x2160 --scale 1.0x1.0 --pos 0x0 --rotate normal --output HDMI-1 --off --output DP-0 --off --output DP-1 --off --output DP-2 --off --output DP-3 --off
